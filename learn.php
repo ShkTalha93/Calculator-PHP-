88ffca51fce1f30e27cd8d052cfec323bb768f3c
Talha <abdulwahab8281@gmail.com>
2023-08-16 12:25:52 +0500

    learn file

---
+++ learn.php
@@ -0,0 +1,12 @@
+<?php
+//Task1
+
+// Here is how print return 1 and echo gives error because it does not return
+$prnt = print "Print returned: ";
+print($prnt);
+
+// $ech=echo "Echo returned";
+// -----------------Task1 completed---------------------------------------
+
+
+?>
